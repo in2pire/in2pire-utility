fb2fd1491cc341a328cd563940f0d59d70b1fd92
Nhat Tran <thmnhat@gmail.com>
2014-12-14 12:17:28 +0700

    Initial commit

---
+++ src/In2pire/Component/Utility/String.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @file
+ *
+ * @package In2pire
+ * @subpackage MemcachedCli
+ * @author Nhat Tran <nhat.tran@inspire.vn>
+ */
+
+namespace In2pire\Component\Utility;
+
+/**
+ * String utility.
+ */
+class String
+{
+    /**
+     * Convert a string to camel case.
+     *
+     * @param string $text
+     *   String needs to be converted.
+     * @param boolean $ucfirst
+     *   Uppercase first letter or not.
+     *
+     * @return string
+     *   Camelize string.
+     */
+    public static function convertToCamelCase($text, $ucfirst = true)
+    {
+        $text = preg_replace_callback('#[_-]([a-z])#i', function ($matches) {
+            return strtoupper($matches[1]);
+        }, strtolower($text));
+
+        return $ucfirst ? ucfirst($text) : $text;
+    }
+
+    public static function namespacize($list, $namespace = null)
+    {
+        $return = [];
+
+        foreach ($list as $key => $value) {
+            $key = $namespace . $key;
+
+            if (is_array($value)) {
+                $return += static::namespacize($value, $key . '.');
+            } else {
+                $return[$key] = $value;
+            }
+        }
+
+        return $return;
+    }
+
+    public static function hyphenize($text)
+    {
+        $text = preg_replace('#[^a-z0-9]#', '-', strtolower($text));
+        $text = preg_replace('#-{2,}#', '-', $text);
+        return $text;
+    }
+}
